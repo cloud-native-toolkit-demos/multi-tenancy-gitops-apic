428bb0382a30b0cb4f593bd6cfd562129c5527df
Jesús Almaraz Hernández <jesusalmarazhernandez@Jesuss-MacBook-Pro.local>
2021-07-21 11:23:10 +0200

    parametrize git repo

--- scripts/set-git-source.sh
+++ scripts/set-git-source.sh
@@ -9,7 +9,14 @@ GIT_USER=${GIT_USER}
 if [ -z ${GIT_USER} ]; then echo "Please set GIT_USER when running script, optional GIT_BASEURL and GIT_REPO to formed the git url GIT_BASEURL/GIT_USER/GIT_REPO"; exit 1; fi
 set -u
 
-GIT_REPO=${GIT_REPO:-multi-tenancy-gitops.git}
+# Change this value based on the repo you are working with:
+#
+# For example:
+#
+# GIT_REPO=${GIT_REPO:-multi-tenancy-gitops-infra.git}
+# GIT_REPO=${GIT_REPO:-multi-tenancy-gitops-services.git}
+# GIT_REPO=${GIT_REPO:-multi-tenancy-gitops-apps.git}
+GIT_REPO=${GIT_REPO:-multi-tenancy-gitops-infra.git}
 GIT_BASEURL=${GIT_BASEURL:-https://github.com}
 GIT_HOST=${GIT_HOST:-github.com}
 GIT_BRANCH=${GIT_BRANCH:-master}
@@ -22,9 +29,9 @@ find ${SCRIPTDIR}/../0-bootstrap -name '*.yaml' -print0 |
   while IFS= read -r -d '' File; do
     if grep -q "kind: Application\|kind: AppProject" "$File"; then
       echo "$File"
-      sed -i'.bak' -e "s#repoURL: https://github.com/cloud-native-toolkit/multi-tenancy-gitops.git#repoURL: ${GIT_BASEURL}/${GIT_USER}/${GIT_REPO}#" $File
-      sed -i'.bak' -e "s#repoURL: github.com/cloud-native-toolkit/multi-tenancy-gitops.git#repoURL: ${GIT_HOST}/${GIT_USER}/${GIT_REPO}#" $File
-      sed -i'.bak' -e "s#- https://github.com/cloud-native-toolkit/multi-tenancy-gitops.git#- ${GIT_BASEURL}/${GIT_USER}/${GIT_REPO}#" $File
+      sed -i'.bak' -e "s#repoURL: https://github.com/cloud-native-toolkit/${GIT_REPO}#repoURL: ${GIT_BASEURL}/${GIT_USER}/${GIT_REPO}#" $File
+      sed -i'.bak' -e "s#repoURL: github.com/cloud-native-toolkit/${GIT_REPO}#repoURL: ${GIT_HOST}/${GIT_USER}/${GIT_REPO}#" $File
+      sed -i'.bak' -e "s#- https://github.com/cloud-native-toolkit/${GIT_REPO}#- ${GIT_BASEURL}/${GIT_USER}/${GIT_REPO}#" $File
       sed -i'.bak' -e "s#targetRevision: ${GIT_FROM_BRANCH}#targetRevision: ${GIT_BRANCH}#" $File
       rm "${File}.bak"
     fi
